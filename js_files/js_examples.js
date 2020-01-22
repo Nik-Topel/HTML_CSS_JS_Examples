2d6d6b03a8eb4de6b4b05fde2e387b4541d8f1aa
Nik-Topel <niktopel@gmail.com>
2020-01-22 21:50:57 +0700

    was changed bool removed

--- js_files/js_examples.js
+++ js_files/js_examples.js
@@ -1,14 +1,6 @@
-var text_was_changed = false;
-var img_was_changed = false;
 function change_text(){
-    if(text_was_changed ==false){
-        document.getElementById("texttochange").textContent  = "Good job";
-        text_was_changed = true;
-    }
+    document.getElementById("texttochange").textContent  = "Good job";
 }
 function change_image(){
-    if(img_was_changed ==false){
-    document.getElementsByClassName('image_1').scr = "../image_files/tick."png";
-    img_was_changed = true;
-    }
+    document.getElementById("tick").scr =escape("..\image_files\tick.png");
 }
