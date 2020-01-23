dab3759146db8a9229e4c32c31d2740aa407eab4
Nik-Topel <niktopel@gmail.com>
2020-01-23 18:54:58 +0700

    added timer, random color change and the iframe page resets to 1 every time you load it

--- js_files/js_examples.js
+++ js_files/js_examples.js
@@ -1,3 +1,6 @@
+function start(){
+    update_timer();
+}
 function change_text(){
     document.getElementById("texttochange").textContent  = "Good job";
 }
@@ -9,4 +12,51 @@ function random_text(){
 }
 function add_to_number(){
     document.getElementById("add_to_number").textContent = parseInt(document.getElementById("add_to_number").textContent,10) +1;
+}
+function change_color(){
+    document.getElementById("change_color").style.color = "#"+rgbToHex(Math.round(Math.random()*100))+rgbToHex(Math.round(Math.random()*255))+rgbToHex(Math.round(Math.random()*255));
+}
+var rgbToHex = function (rgb) { 
+    var hex = Number(rgb).toString(16);
+    if (hex.length < 2) {
+         hex = "0" + hex;
+    }
+    return hex;
+  };
+var ms = 0;
+var s= 0;
+var m= 0;
+var h = 0;
+var d = 0;
+var w = 0;
+function pad2(number) {
+    return (number < 10 ? '0' : '') + number;
+}
+function update_timer()
+{
+    setTimeout(function(){
+    ms += 1;
+    if(ms == 100){
+        s += 1;
+        ms = 0;
+    }
+    if(s == 60){
+        m += 1;
+        s = 60;
+    }
+    if(m == 60){
+        h += 1;
+        m = 0
+    }
+    if(h == 24){
+        d += 1;
+        h = 0
+    }
+    if(d == 7){
+        w += 1;
+        d = 0
+    }
+    document.getElementById("timer").textContent = pad2(w) +":"+ pad2(d) +":"+ pad2(h) +":"+ pad2(m) +":"+ pad2(s) +":"+ pad2(ms); 
+    update_timer(); 
+    },10)
 }
--- js_files/main.js
+++ js_files/main.js
@@ -1,31 +1,22 @@
-var html_string= "html_files/html_examples.html";
-var css_string= "html_files/css_examples.html";
-var js_string= "html_files/js_examples.html";
 var loaded_page = 1;
 function start(){
-    if(document.getElementById('iframe_1').src == html_string){
-        loaded_page = 1;
-    }else if(document.getElementById('iframe_1').src == css_string){
-        loaded_page = 2;
-    }else if(document.getElementById('iframe_1').src == js_string){
-        loaded_page = 3;
-    }
+    document.getElementById('iframe_1').src = escape("html_files/html_examples.html");
 }
 function set_iframe_to_html(){
     if(loaded_page != 1){
-        document.getElementById('iframe_1').src = escape(html_string);
+        document.getElementById('iframe_1').src = escape("html_files/html_examples.html");
         loaded_page = 1;
-    }
+    } 
 }
 function set_iframe_to_css(){
     if(loaded_page != 2){
-        document.getElementById('iframe_1').src = escape(css_string);
+        document.getElementById('iframe_1').src = escape("html_files/css_examples.html");
         loaded_page = 2;
     }
 }
 function set_iframe_to_js(){
     if(loaded_page != 3){
-        document.getElementById('iframe_1').src = escape(js_string);
+        document.getElementById('iframe_1').src = escape("html_files/js_examples.html");
         loaded_page = 3;
     }
 }
