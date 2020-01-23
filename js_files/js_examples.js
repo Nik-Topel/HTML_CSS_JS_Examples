810ad056c315292e94e26a0fe585447583abe48c
Nik-Topel <niktopel@gmail.com>
2020-01-23 17:09:14 +0700

    Random generator and addition

--- js_files/js_examples.js
+++ js_files/js_examples.js
@@ -4,3 +4,9 @@ function change_text(){
 function change_image(){
     document.getElementById("tick").src = "../image_files/tick.png"
 }
+function random_text(){
+    document.getElementById("random_number").textContent = Math.round(Math.random()*100);
+}
+function add_to_number(){
+    document.getElementById("add_to_number").textContent = parseInt(document.getElementById("add_to_number").textContent,10) +1;
+}
